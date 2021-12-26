f81d79a0984686c024d80f43a49dc75be3c61ba9
Nidal Hjab <NHjab@asaltech.com>
2021-12-26 20:52:24 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@nidalhjab/button-comp';
+import testString from '@nidalhjab/ui';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button label={"2nd-app"}/>
+      <p>{testString}</p>
     </div>
   );
 }
